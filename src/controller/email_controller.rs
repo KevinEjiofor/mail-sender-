1ec82aa2e4bfcc0bbb3170b09b000c669dd178a1
Ejiofor E. Kevin <ejioforkelvin@gmail.com>
2025-03-30 20:20:22 +0100

    first commit

---
+++ src/controller/email_controller.rs
@@ -0,0 +1,22 @@
+use crate::service::email_service::EmailService;
+
+pub struct EmailController {
+    service: EmailService,
+}
+
+impl EmailController {
+    pub fn new(service: EmailService) -> Self {
+        Self { service }
+    }
+
+    pub async fn send_test_email(&self) {
+        let recipient = "recipient@example.com";
+        let subject = "Test Email from Rust";
+        let content = "<p>Hello! This is a test email sent using Brevo in Rust.</p>";
+
+        match self.service.send_email(recipient, subject, content).await {
+            Ok(_) => println!("Email sent successfully!"),
+            Err(e) => eprintln!("Failed to send email: {}", e),
+        }
+    }
+}
